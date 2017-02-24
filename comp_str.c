f2848ad96bf06f39c027dafbb3dcba7957a96741
skkif1 <skkif1@gmail.com>
2017-02-24 02:58:08 +0200

    refactor repo

---
+++ comp_str.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strn_cmp.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omotyliu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/22 18:53:51 by omotyliu          #+#    #+#             */
+/*   Updated: 2016/11/22 18:53:52 by omotyliu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *first, const char *second, size_t num)
+{
+	unsigned char a;
+	unsigned char b;
+
+	a = *first;
+	b = *second;
+	while (a == b && num > 0)
+	{
+		a = *first++;
+		b = *second++;
+		if (a == '\0')
+		{
+			return (0);
+		}
+		num--;
+	}
+	return ((int)a - b);
+}
